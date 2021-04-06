721f455ba511f2453bc4d9f942284d5977405a59
Alway09 <alvvay09@gmail.com>
2021-04-06 14:08:46 +1000

    first commit

---
+++ project_build/tests/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/tests"
+  "../bin/tests.pdb"
+  "CMakeFiles/tests.dir/src/main.cpp.o"
+  "CMakeFiles/tests.dir/src/main.cpp.o.d"
+  "CMakeFiles/tests.dir/src/test_cases/sum_tests.cpp.o"
+  "CMakeFiles/tests.dir/src/test_cases/sum_tests.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ project_src/server/src/main.cpp
@@ -0,0 +1,10 @@
+﻿#include "udecoder/dummy.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "85496736897 + 94583068 = "
+		<< udecoder::sum("85496736897", "94583068")
+		<< std::endl;
+	return 0;
+}
